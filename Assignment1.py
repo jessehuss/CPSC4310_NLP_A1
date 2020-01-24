d404e847270522c2f31e95d00a0b25f729f4d30f
jesseh <jesseh@rdtminc.com>
2020-01-24 14:51:31 -0700

    finished

--- Assignment1.py
+++ Assignment1.py
@@ -1,19 +1,22 @@
-from nltk.corpus import brown
+from nltk.corpus import brown, stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer, LancasterStemmer
+from nltk import WordNetLemmatizer
 
-count = 0
+stopWords = set(stopwords.words('english'))
+wln = WordNetLemmatizer()
+lemmatizer = WordNetLemmatizer()
+porter = PorterStemmer()
+lancaster = LancasterStemmer()
 
 for cat in brown.categories():
-    print (cat + ' has ' + str(len(brown.words(categories=cat))) + ' word tokens.')
-    print (cat + ' has ' + str(len(set(brown.words(categories=cat)))) + ' word types.')
-    count += len(brown.words(categories=cat))
+    words = brown.words(categories=cat)
+    noStopWords = [nsw for nsw in words if nsw not in stopWords]
+    lemmatizedWords = [wln.lemmatize(lw) for lw in words]
+    pstemmedWords = [porter.stem(psw) for psw in words]
+    lstemmedWords = [lancaster.stem(lsw) for lsw in words]
+    print (cat.upper() + ':')
+    print (f'Word Tokens:\n{str(len(words))} vanilla.\n{str(len(noStopWords))} no stop words.\n{str(len(lemmatizedWords))} lemmatized.\n{str(len(pstemmedWords))} porter stemmed.\n{str(len(lstemmedWords))} lancaster scanned.\n' )
+    print (f'Word Types:\n{str(len(set(words)))} vanilla.\n{str(len(set(noStopWords)))} no stop words.\n{str(len(set(lemmatizedWords)))} lemmatized.\n{str(len(set(pstemmedWords)))} porter stemmed.\n{str(len(set(lstemmedWords)))} lancaster scanned.\n' )
 
-print ('Vocabulary size of the entire corpus is: ' + str(len(brown.words())) - 1 + ' and the sum of words from each category is: ' + str(count) - 1)
-
-# How many word tokens does each category/genre have?
-# How many word types does each category/genre have?
-# What is the vocabulary size of the whole corpus?
-
-# Variation chooser:
-# a) with/without stopwords
-# b) with/without lemmatization
-# c) with/without stemming
+print (f'Vocabulary Size:\n{str(len(brown.words()))} vanilla.\n{str(len([word for word in brown.words() if word not in stopWords]))} no stop words.\n{str(len([wln.lemmatize(w) for w in brown.words()]))} lemmatized.\n{str(len([porter.stem(w) for w in brown.words()]))} porter stemmed.\n{str(len([lancaster.stem(w) for w in brown.words()]))} lancaster scanned.' )
