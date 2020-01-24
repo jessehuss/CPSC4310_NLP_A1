c5b5da18d6e0e9855803fce20df851ee24497251
jesseh <jesseh@rdtminc.com>
2020-01-23 17:57:28 -0700

    progress

--- Assignment1.py
+++ Assignment1.py
@@ -1,7 +1,13 @@
 from nltk.corpus import brown
 
+count = 0
+
 for cat in brown.categories():
-    print (cat + ' has ' + str(len(brown.words(categories=cat))) + ' words.')
+    print (cat + ' has ' + str(len(brown.words(categories=cat))) + ' word tokens.')
+    print (cat + ' has ' + str(len(set(brown.words(categories=cat)))) + ' word types.')
+    count += len(brown.words(categories=cat))
+
+print ('Vocabulary size of the entire corpus is: ' + str(len(brown.words())) - 1 + ' and the sum of words from each category is: ' + str(count) - 1)
 
 # How many word tokens does each category/genre have?
 # How many word types does each category/genre have?
