c030f6c1fa0a996e2c11c3a913b674efa14a04bc
jesseh <jesseh@rdtminc.com>
2020-01-24 15:03:58 -0700

    fix

--- Assignment1.py
+++ Assignment1.py
@@ -16,7 +16,7 @@ for cat in brown.categories():
     pstemmedWords = [porter.stem(psw) for psw in words]
     lstemmedWords = [lancaster.stem(lsw) for lsw in words]
     print (cat.upper() + ':')
-    print (f'Word Tokens:\n{str(len(words))} vanilla.\n{str(len(noStopWords))} no stop words.\n{str(len(lemmatizedWords))} lemmatized.\n{str(len(pstemmedWords))} porter stemmed.\n{str(len(lstemmedWords))} lancaster scanned.\n' )
-    print (f'Word Types:\n{str(len(set(words)))} vanilla.\n{str(len(set(noStopWords)))} no stop words.\n{str(len(set(lemmatizedWords)))} lemmatized.\n{str(len(set(pstemmedWords)))} porter stemmed.\n{str(len(set(lstemmedWords)))} lancaster scanned.\n' )
+    print (f'Word Tokens:\n{str(len(words))} vanilla.\n{str(len(noStopWords))} no stop words.\n{str(len(lemmatizedWords))} lemmatized.\n{str(len(pstemmedWords))} porter stemmed.\n{str(len(lstemmedWords))} lancaster scanned.\n')
+    print (f'Word Types:\n{str(len(set(words)))} vanilla.\n{str(len(set(noStopWords)))} no stop words.\n{str(len(set(lemmatizedWords)))} lemmatized.\n{str(len(set(pstemmedWords)))} porter stemmed.\n{str(len(set(lstemmedWords)))} lancaster scanned.\n')
 
-print (f'Vocabulary Size:\n{str(len(brown.words()))} vanilla.\n{str(len([word for word in brown.words() if word not in stopWords]))} no stop words.\n{str(len([wln.lemmatize(w) for w in brown.words()]))} lemmatized.\n{str(len([porter.stem(w) for w in brown.words()]))} porter stemmed.\n{str(len([lancaster.stem(w) for w in brown.words()]))} lancaster scanned.' )
+print (f'Vocabulary Size:\n{str(len(brown.words()))} vanilla.\n{str(len([word for word in brown.words() if word not in stopWords]))} no stop words.\n{str(len([wln.lemmatize(w) for w in brown.words()]))} lemmatized.\n{str(len([porter.stem(w) for w in brown.words()]))} porter stemmed.\n{str(len([lancaster.stem(w) for w in brown.words()]))} lancaster scanned.')
